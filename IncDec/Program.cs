b08035353668f039c38f4090ff132455d4164247
StarryNight00 <cisa.matias@gmail.com>
2019-03-15 09:22:07 +0000

    Ex1-1

--- IncDec/Program.cs
+++ IncDec/Program.cs
@@ -6,7 +6,26 @@ namespace IncDec
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            // Mudificar para mostrar a e b antes de cada operação
+            int a = 5;
+            int b = -6;
+            int c;
+            int d;
+            int e;
+            int f;
+
+            c = a++ + b++;
+            d = ++a - ++b;
+            e = a-- + ++b;
+            f = --a - --b;
+
+            Console.WriteLine(a);
+            Console.WriteLine(b);
+            Console.WriteLine("\n\n Resultados");
+            Console.WriteLine(c);
+            Console.WriteLine(d);
+            Console.WriteLine(e);
+            Console.WriteLine(f);
         }
     }
 }
