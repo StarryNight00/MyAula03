9ee8e8511e2deaea0caf642b59aa1da27ee9b543
StarryNight00 <cisa.matias@gmail.com>
2019-03-15 11:46:47 +0000

    Ex1-//  +Ex2  +Ex3

---
+++ ChatBot/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+
+namespace ChatBot
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            bool e = false;
+            while (e != true)
+            {
+                Console.WriteLine("Hello, Im ChatBot!");
+                // Variáveis auxiliares
+                string question, response;
+
+                // Colocar questão ao utilizador
+                Console.Write("What's your question? ");
+                question = Console.ReadLine();
+
+                // Verificar se se trata de uma questão conhecida e determinar uma
+                // resposta adequada
+                switch (question)
+                {
+                    case "How are you?":
+                        response = "I'm fine, thank you";
+                        break;
+                    case "What's your name?":
+                        response = "ChatBot.";
+                        break;
+                    case "What's your mission?":
+                        response = "Destroy mankind!";
+                        break;
+                    case "Who are you?":
+                        response = "ChatBot... like I said... minutes ago.";
+                        break;
+                    case "What are you)":
+                        response = "I'm a conversation-bot from #%&/*%& Inc.";
+                        break;
+                    case "What's your favorite color?":
+                        response = "It's beyond your light spectrum perception. Don't bother, Human.";
+                        break;
+                    case "EXIT":
+                        response = "TERMINATION OF CHAT";
+                        e = true;
+                        break;
+                    default:
+                        response = "I don't know what you mean.";
+                        break;
+                }
+
+            }
+        }
+    }
+}
---
+++ Piramide/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace Piramide
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Declaração de variáveis
+            string nLinhas;
+            int n;
+
+            //Pedir num. de linhas ao utilizador
+            Console.WriteLine("Quantas linhas?");
+            nLinhas = Console.ReadLine();
+            n = Convert.ToInt32(nLinhas);
+            
+            //Ciclo
+            for (int i = 0; i < n; i++)
+            {
+                //Declaração de variáveis
+                int esp, ast;
+
+                //Determinar qunts espaços  # n - i - 1
+                esp = n - i - 1;
+
+                //Determinar qunts **  # i * 2 + 1 (pascal??)
+                ast = i * 2 + 1;
+
+                //Imprimir espaços
+                for (int j = 0; j < esp; j++)
+                {
+                    Console.Write(" ");
+                }
+                //Imprimir **
+                for (int j = 0; j < ast; j++)
+                {
+                    Console.Write("*");
+                }
+
+                //Nova linha vazia
+                Console.WriteLine(); //altern: Console.Write("\n");
+            }
+        }
+    }
+}
